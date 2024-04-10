41c92b3d7bbc0a54fd92be5d96b59090796d5f4a
JNico07 <nicoolano43@gmail.com>
2024-04-10 23:14:49 +0800

    add Buttons on Monitor mode: Start/Pause (measure screen-time) and Display (camera view) buttons.

---
+++ app/src/main/java/org/tensorflow/lite/examples/detection/ReaWriteUserDetails.java
@@ -0,0 +1,11 @@
+package org.tensorflow.lite.examples.detection;
+
+public class ReaWriteUserDetails {
+    public String full_name;
+
+    public ReaWriteUserDetails(String full_name) {
+        this.full_name = full_name;
+    }
+
+
+}
