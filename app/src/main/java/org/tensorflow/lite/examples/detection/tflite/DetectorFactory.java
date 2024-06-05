f5fe031e4a4aae9f52cdb40d92d33d8ef06cf749
JNico07 <nicoolano43@gmail.com>
2024-06-05 16:01:03 +0800

    small file changes on assets folder

--- app/src/main/java/org/tensorflow/lite/examples/detection/tflite/DetectorFactory.java
+++ app/src/main/java/org/tensorflow/lite/examples/detection/tflite/DetectorFactory.java
@@ -16,68 +16,48 @@ public class DetectorFactory {
         int[][] masks = new int[][]{{0}};
         int[] anchors = new int[]{0};
 
-        if (modelFilename.equals("yolov5s.tflite")) {
-            labelFilename = "file:///android_asset/customclasses.txt";
-            isQuantized = false;
-            inputSize = 416;
-            output_width = new int[]{80, 40, 20};
-            masks = new int[][]{{0, 1, 2}, {3, 4, 5}, {6, 7, 8}};
-            anchors = new int[]{
-                    10,13, 16,30, 33,23, 30,61, 62,45, 59,119, 116,90, 156,198, 373,326
-            };
-        }
-        else if (modelFilename.equals("best-fp16.tflite")) {
-            labelFilename = "file:///android_asset/customclasses.txt";
-            isQuantized = false;
-            inputSize = 416;
-            output_width = new int[]{40, 20, 10};
-            masks = new int[][]{{0, 1, 2}, {3, 4, 5}, {6, 7, 8}};
-            anchors = new int[]{
-                    10,13, 16,30, 33,23, 30,61, 62,45, 59,119, 116,90, 156,198, 373,326
-            };
-        }
-        else if (modelFilename.equals("best-fp16_v1.tflite")) {
-            labelFilename = "file:///android_asset/customclasses_v1.txt";
-            isQuantized = false;
-            inputSize = 416;
-            output_width = new int[]{40, 20, 10};
-            masks = new int[][]{{0, 1, 2}, {3, 4, 5}, {6, 7, 8}};
-            anchors = new int[]{
-                    10,13, 16,30, 33,23, 30,61, 62,45, 59,119, 116,90, 156,198, 373,326
-            };
-        }
-        else if (modelFilename.equals("best-fp16_v2.tflite")) {
-            labelFilename = "file:///android_asset/customclasses_v2.txt";
-            isQuantized = false;
-            inputSize = 416;
-            output_width = new int[]{40, 20, 10};
-            masks = new int[][]{{0, 1, 2}, {3, 4, 5}, {6, 7, 8}};
-            anchors = new int[]{
-                    10,13, 16,30, 33,23, 30,61, 62,45, 59,119, 116,90, 156,198, 373,326
-            };
-        }
-        else if (modelFilename.equals("best-fp16_v3.tflite")) {
-            labelFilename = "file:///android_asset/customclasses_v3.txt";
-            isQuantized = false;
-            inputSize = 640;
-            output_width = new int[]{40, 20, 10};
-            masks = new int[][]{{0, 1, 2}, {3, 4, 5}, {6, 7, 8}};
-            anchors = new int[]{
-                    10,13, 16,30, 33,23, 30,61, 62,45, 59,119, 116,90, 156,198, 373,326
-            };
-        }
-        else if (modelFilename.equals("yolov5s-int8.tflite")) {
-            labelFilename = "file:///android_asset/customclasses.txt";
-            isQuantized = true;
-            inputSize = 416;
-            output_width = new int[]{40, 20, 10};
-            masks = new int[][]{{0, 1, 2}, {3, 4, 5}, {6, 7, 8}};
-            anchors = new int[]{
-                    10,13, 16,30, 33,23, 30,61, 62,45, 59,119, 116,90, 156,198, 373,326
-            };
-        }
+        modelFilename.equals("model(s).tflite");
+        labelFilename = "file:///android_asset/customclasses.txt";
+        isQuantized = false;
+        inputSize = 416;
+        output_width = new int[]{80, 40, 20};
+        masks = new int[][]{{0, 1, 2}, {3, 4, 5}, {6, 7, 8}};
+        anchors = new int[]{
+                10,13, 16,30, 33,23, 30,61, 62,45, 59,119, 116,90, 156,198, 373,326};
+
+//        if (modelFilename.equals("model(s).tflite")) {
+//            labelFilename = "file:///android_asset/customclasses.txt";
+//            isQuantized = false;
+//            inputSize = 416;
+//            output_width = new int[]{80, 40, 20};
+//            masks = new int[][]{{0, 1, 2}, {3, 4, 5}, {6, 7, 8}};
+//            anchors = new int[]{
+//                    10,13, 16,30, 33,23, 30,61, 62,45, 59,119, 116,90, 156,198, 373,326
+//            };
+//        }
+//        else if (modelFilename.equals("model(s.1).tflite")) {
+//            labelFilename = "file:///android_asset/customclasses.txt";
+//            isQuantized = false;
+//            inputSize = 416;
+//            output_width = new int[]{40, 20, 10};
+//            masks = new int[][]{{0, 1, 2}, {3, 4, 5}, {6, 7, 8}};
+//            anchors = new int[]{
+//                    10,13, 16,30, 33,23, 30,61, 62,45, 59,119, 116,90, 156,198, 373,326
+//            };
+//        }
+//        else if (modelFilename.equals("model(n).tflite")) {
+//            labelFilename = "file:///android_asset/customclasses.txt";
+//            isQuantized = false;
+//            inputSize = 416;
+//            output_width = new int[]{40, 20, 10};
+//            masks = new int[][]{{0, 1, 2}, {3, 4, 5}, {6, 7, 8}};
+//            anchors = new int[]{
+//                    10,13, 16,30, 33,23, 30,61, 62,45, 59,119, 116,90, 156,198, 373,326
+//            };
+//        }
+        assert labelFilename != null;
         return YoloV5Classifier.create(assetManager, modelFilename, labelFilename, isQuantized,
                 inputSize);
     }
-
 }
+
