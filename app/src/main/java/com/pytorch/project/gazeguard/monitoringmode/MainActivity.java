504dbd803406ba0bbe0991e3bae1edde851d523a
JNico07 <nicoolano43@gmail.com>
2024-11-22 17:21:04 +0800

    - fix bugs - remove some unused imports - add cff file

--- app/src/main/java/com/pytorch/project/gazeguard/monitoringmode/MainActivity.java
+++ app/src/main/java/com/pytorch/project/gazeguard/monitoringmode/MainActivity.java
@@ -31,9 +31,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
-import android.widget.LinearLayout;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
